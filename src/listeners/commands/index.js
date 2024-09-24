07eca8417c9b10c27faa2ef3014050503b95b07d
Aaron Campecino <aaroncampecino@gmail.com>
2024-09-24 17:49:24 -0600

    additional command

--- src/listeners/commands/index.js
+++ src/listeners/commands/index.js
@@ -1,11 +1,15 @@
 // import { issueCommand } from "./issue-command.js";
 
-import { adminChannelCommand } from "./admin-channel-command.js";
+// import { adminChannelCommand } from "./admin-channel-command.js";
+import { financeChannelCommand } from "./finance-channel-command.js";
+import { supportChannelCommand } from "./support-channel-command.js";
 
 export const commandRegister = (app) => {
   // app.command("/issue", issueCommand);
 
-  app.command("/bot-admin", adminChannelCommand);
+  // app.command("/bot-admin", adminChannelCommand);
+  app.command("/finance-bot", financeChannelCommand);
+  app.command("/support-bot", supportChannelCommand);
   // app.command("/bot-finance", issueCommand);
   // app.command("/bot-support", issueCommand);
   // app.command("/bot-website", issueCommand);
